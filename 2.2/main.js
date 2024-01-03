0e5bda4fb630c16d6e289a6d5473628c545bb1ef
mr1noy <to.1noy@gmail.com>
2024-01-03 16:00:14 +0300

    2.2 v2

--- 2.2/main.js
+++ 2.2/main.js
@@ -3,3 +3,7 @@ let city = 'Sevastopol';
 let age = 39;
 let hobby = 'web design';
 
+console.log ('Hello,',firstName);
+console.log ('My sity is', city);
+console.log ('I am', age);
+console.log ('my hobby is', hobby);
--- 2.3/main.js
+++ 2.3/main.js
@@ -1,6 +1,12 @@
 const titleProject = 'newproject-90000';
+console.log (typeof titleProject);
 const screensValue = 'tempates, with_unique_design, with_animations';
+console.log (typeof screensValue);
 const screenPrice = 10000;
+console.log (typeof screenPrice);
 const percentage = 10;
+console.log (typeof percentage);
 const fullPrice = 80000;
+console.log (typeof fullPrice);
 const responsive = true;
+console.log (typeof responsive);
