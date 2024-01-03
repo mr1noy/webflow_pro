118811205b6f2b07da836096d1a02f2b46769b99
mr1noy <to.1noy@gmail.com>
2024-01-03 18:04:43 +0300

    2.3 v3

--- 2.3/main.js
+++ 2.3/main.js
@@ -1,13 +1,13 @@
-const titleProject = 'newproject-90000';
+let titleProject = 'Корпоративный сайт с квизом';
 console.log (typeof titleProject);
-const screensValue = 'tempates, with_unique_design, with_animations';
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
 console.log (typeof screensValue);
-const screenPrice = 10000;
+let screenPrice = 10000;
 console.log (typeof screenPrice);
-const percentage = 10;
+let percentage = 10;
 console.log (typeof percentage);
-const fullPrice = 80000;
+let fullPrice = 80000;
 console.log (typeof fullPrice);
-const responsive = true;
+let responsive = true;
 console.log (typeof responsive);
 
