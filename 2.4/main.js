856ce86dff568d80dbcf877c5bf4a03f4ff73bf8
mr1noy <to.1noy@gmail.com>
2024-01-03 18:12:53 +0300

    les-2-4

---
+++ 2.4/main.js
@@ -0,0 +1,13 @@
+let titleProject = 'Корпоративный сайт с квизом';
+console.log (typeof titleProject);
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+console.log (typeof screensValue);
+let screenPrice = 10000;
+console.log (typeof screenPrice);
+let percentage = 10;
+console.log (typeof percentage);
+let fullPrice = 80000;
+console.log (typeof fullPrice);
+let responsive = true;
+console.log (typeof responsive);
+
